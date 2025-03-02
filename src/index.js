8c434d3341d569d2c13314c0fa9e58916e6496fe
Shravan <sravankumargoud555@gmail.com>
2025-03-02 13:44:18 +0530

    hh

---
+++ src/index.js
@@ -0,0 +1,23 @@
+export default {
+  async fetch(request) {
+    return new Response("Hello from Cloudflare Worker!", {
+      headers: { "Content-Type": "text/plain" },
+    });
+  },
+};
+
+addEventListener("fetch", (event) => {
+  event.respondWith(handleRequest(event.request));
+});
+
+async function handleRequest(request) {
+  return new Response("Hello, world!", { status: 200 });
+}
+addEventListener("scheduled", (event) => {
+  event.waitUntil(runTask());
+});
+
+async function runTask() {
+  console.log("Scheduled task running...");
+}
+
